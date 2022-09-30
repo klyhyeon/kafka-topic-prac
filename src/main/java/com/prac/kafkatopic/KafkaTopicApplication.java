02fd6b91bc8cb4d8a4e756e1b262618d6bdb7a61
baekho <klyhyeon@gmail.com>
2022-09-30 10:10:59 +0900

    inital commit

---
+++ src/main/java/com/prac/kafkatopic/KafkaTopicApplication.java
@@ -0,0 +1,13 @@
+package com.prac.kafkatopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTopicApplication.class, args);
+	}
+
+}
